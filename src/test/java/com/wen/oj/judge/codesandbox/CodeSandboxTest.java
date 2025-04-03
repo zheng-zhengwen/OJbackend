c5754f50705be4168b1f5b99e9355420654bf980
“awen” <55828784@qq.com>
2025-04-03 15:40:16 +0800

    完成后端基本所有接口，主页接口补充

--- src/test/java/com/wen/oj/judge/codesandbox/CodeSandboxTest.java
+++ src/test/java/com/wen/oj/judge/codesandbox/CodeSandboxTest.java
@@ -1,11 +1,8 @@
 package com.wen.oj.judge.codesandbox;
 
-import com.wen.oj.judge.codesandbox.impl.ExampleCodeSandbox;
-import com.wen.oj.judge.codesandbox.impl.RemoteCodeSandbox;
 import com.wen.oj.judge.codesandbox.model.ExecuteCodeRequest;
 import com.wen.oj.judge.codesandbox.model.ExecuteCodeResponse;
-import com.wen.oj.model.entity.QuestionSubmit;
-import com.wen.oj.model.enums.QuestionSubmitLanguageEnum;
+import com.wen.oj.model.enums.QuestionsSubmitLanguageEnum;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Value;
@@ -21,12 +18,12 @@ class CodeSandboxTest {
     @Value("${codesandbox.type:example}")
     private String type;
 
-    @Test
+//    @Test
     void executeCode() {
         CodeSandbox codeSandbox = CodeSandboxFactory.newInstance(type);
         String code = "int main(){}";
-        String language = QuestionSubmitLanguageEnum.JAVA.getValue();
-        List<String> inputList = Arrays.asList("1 2", "3 4");
+        String language = QuestionsSubmitLanguageEnum.JAVA.getValue();
+        List<String> inputList = Arrays.asList("1 2", "3");
         ExecuteCodeRequest executeCodeRequest = ExecuteCodeRequest.builder()
                 .code(code)
                 .language(language)
@@ -36,11 +33,11 @@ class CodeSandboxTest {
         Assertions.assertNotNull(executeCodeResponse);
     }
 
-    @Test
+//    @Test
     void executeCodeByValue() {
         CodeSandbox codeSandbox = CodeSandboxFactory.newInstance(type);
         String code = "int main() {}";
-        String language = QuestionSubmitLanguageEnum.JAVA.getValue();
+        String language = QuestionsSubmitLanguageEnum.JAVA.getValue();
         List<String> inputList = Arrays.asList("1 2", "3 4");
         ExecuteCodeRequest executeCodeRequest = ExecuteCodeRequest.builder()
                 .code(code)
@@ -62,8 +59,38 @@ class CodeSandboxTest {
                 "        System.out.println(\"结果:\" + (a + b));\n" +
                 "    }\n" +
                 "}\n";
-        String language = QuestionSubmitLanguageEnum.JAVA.getValue();
-        List<String> inputList = Arrays.asList("1 2", "3 4");
+        String language = QuestionsSubmitLanguageEnum.JAVA.getValue();
+        List<String> inputList = Arrays.asList("1 2", "3");
+        ExecuteCodeRequest executeCodeRequest = ExecuteCodeRequest.builder()
+                .code(code)
+                .language(language)
+                .inputList(inputList)
+                .build();
+        ExecuteCodeResponse executeCodeResponse = codeSandbox.executeCode(executeCodeRequest);
+        Assertions.assertNotNull(executeCodeResponse);
+    }
+
+    @Test
+    void executeCodeByProxy2() {
+        CodeSandbox codeSandbox = CodeSandboxFactory.newInstance(type);
+        codeSandbox = new CodeSandboxProxy(codeSandbox);
+        String code = "public class Main {\n" +
+                "    public static void main(String[] args) {\n" +
+                "        if (args.length < 2) {\n" +
+                "            System.out.println(\"输入参数不足，请提供至少两个整数。\");\n" +
+                "            return;\n" +
+                "        }\n" +
+                "        try {\n" +
+                "            int a = Integer.parseInt(args[0]);\n" +
+                "            int b = Integer.parseInt(args[1]);\n" +
+                "            System.out.println(\"结果:\" + (a + b));\n" +
+                "        } catch (NumberFormatException e) {\n" +
+                "            System.out.println(\"输入不是有效的整数，请检查输入。\");\n" +
+                "        }\n" +
+                "    }\n" +
+                "}\n";
+        String language = QuestionsSubmitLanguageEnum.JAVA.getValue();
+        List<String> inputList = Arrays.asList("1 2", "3");
         ExecuteCodeRequest executeCodeRequest = ExecuteCodeRequest.builder()
                 .code(code)
                 .language(language)
@@ -78,14 +105,124 @@ class CodeSandboxTest {
         CodeSandbox codeSandbox = CodeSandboxFactory.newInstance(type);
         codeSandbox=new CodeSandboxProxy(codeSandbox);
         String code = "#include <iostream>\n" +
-                "int main() {\n" +
-                "    int a, b;\n" +
-                "    std::cin >> a >> b;\n" +
+                "#include <cstdlib> // 用于 atoi\n" +
+                "#include <vector>\n" +
+                "#include <algorithm>\n" +
+                "\n" +
+                "using namespace std;\n" +
+                "\n" +
+                "int trap(vector<int>& height) {\n" +
+                "    if (height.empty()) return 0;\n" +
+                "    \n" +
+                "    int left = 0, right = height.size() - 1;\n" +
+                "    int left_max = height[left], right_max = height[right];\n" +
+                "    int result = 0;\n" +
+                "    \n" +
+                "    while (left < right) {\n" +
+                "        if (left_max < right_max) {\n" +
+                "            left++;\n" +
+                "            if (height[left] < left_max) {\n" +
+                "                result += left_max - height[left];\n" +
+                "            } else {\n" +
+                "                left_max = height[left];\n" +
+                "            }\n" +
+                "        } else {\n" +
+                "            right--;\n" +
+                "            if (height[right] < right_max) {\n" +
+                "                result += right_max - height[right];\n" +
+                "            } else {\n" +
+                "                right_max = height[right];\n" +
+                "            }\n" +
+                "        }\n" +
+                "    }\n" +
+                "    \n" +
+                "    return result;\n" +
+                "}\n" +
+                "\n" +
+                "int main(int argc, char* argv[]) {\n" +
+                "    vector<int> height;\n" +
+                "    for (int i = 1; i < argc; i++) {\n" +
+                "        height.push_back(atoi(argv[i]));\n" +
+                "    }\n" +
+                "    cout << trap(height) << endl;\n" +
+                "    return 0;\n" +
+                "}";
+        String language = QuestionsSubmitLanguageEnum.CPLUSPLUS.getValue();
+        List<String> inputList = Arrays.asList("4 2 0 3 2 5", "9");
+        ExecuteCodeRequest executeCodeRequest = ExecuteCodeRequest.builder()
+                .code(code)
+                .language(language)
+                .inputList(inputList)
+                .build();
+        ExecuteCodeResponse executeCodeResponse = codeSandbox.executeCode(executeCodeRequest);
+        Assertions.assertNotNull(executeCodeResponse);
+    }
+    @Test
+    void executeCodeByProxy4() {
+        CodeSandbox codeSandbox = CodeSandboxFactory.newInstance(type);
+        codeSandbox=new CodeSandboxProxy(codeSandbox);
+        String code = "#include <iostream>\n" +
+                "#include <cstdlib> // 用于 atoi\n" +
+                "\n" +
+                "int main(int argc, char* argv[]) {\n" +
+                "    // 解析参数\n" +
+                "    int a = std::atoi(argv[1]);\n" +
+                "    int b = std::atoi(argv[2]);\n" +
                 "    std::cout << a + b << std::endl;\n" +
                 "    return 0;\n" +
                 "}";
-        String language = QuestionSubmitLanguageEnum.CPLUSPLUS.getValue();
-        List<String> inputList = Arrays.asList("1 2", "3 4");
+        String language = QuestionsSubmitLanguageEnum.CPLUSPLUS.getValue();
+        List<String> inputList = Arrays.asList("4 2", "6");
+        ExecuteCodeRequest executeCodeRequest = ExecuteCodeRequest.builder()
+                .code(code)
+                .language(language)
+                .inputList(inputList)
+                .build();
+        ExecuteCodeResponse executeCodeResponse = codeSandbox.executeCode(executeCodeRequest);
+        Assertions.assertNotNull(executeCodeResponse);
+    }
+    @Test
+    void executeCodeByProxy3() {
+        CodeSandbox codeSandbox = CodeSandboxFactory.newInstance(type);
+        codeSandbox=new CodeSandboxProxy(codeSandbox);
+        String code = "public class Main {\n" +
+                "    public static void main(String[] args) {\n" +
+                "        int[] height = new int[args.length];\n" +
+                "        for (int i = 0; i < args.length; i++) {\n" +
+                "            height[i] = Integer.parseInt(args[i]);\n" +
+                "        }\n" +
+                "        System.out.println(trap(height));\n" +
+                "    }\n" +
+                "\n" +
+                "    public static int trap(int[] height) {\n" +
+                "        if (height == null || height.length == 0) {\n" +
+                "            return 0;\n" +
+                "        }\n" +
+                "        int left = 0, right = height.length - 1;\n" +
+                "        int left_max = 0, right_max = 0;\n" +
+                "        int result = 0;\n" +
+                "        while (left < right) {\n" +
+                "            if (height[left] < height[right]) {\n" +
+                "                if (height[left] >= left_max) {\n" +
+                "                    left_max = height[left];\n" +
+                "                } else {\n" +
+                "                    result += left_max - height[left];\n" +
+                "                }\n" +
+                "                left++;\n" +
+                "            } else {\n" +
+                "                if (height[right] >= right_max) {\n" +
+                "                    right_max = height[right];\n" +
+                "                } else {\n" +
+                "                    result += right_max - height[right];\n" +
+                "                }\n" +
+                "                right--;\n" +
+                "            }\n" +
+                "        }\n" +
+                "        return result;\n" +
+                "    }\n" +
+                "}";
+        String language = QuestionsSubmitLanguageEnum.JAVA.getValue();
+        List<String> inputList = Arrays.asList("4 2 0 3 2 5", "9");
         ExecuteCodeRequest executeCodeRequest = ExecuteCodeRequest.builder()
                 .code(code)
                 .language(language)
@@ -102,7 +239,7 @@ class CodeSandboxTest {
             String type = scanner.next();
             CodeSandbox codeSandbox = CodeSandboxFactory.newInstance(type);
             String code = "int main(){}";
-            String language = QuestionSubmitLanguageEnum.JAVA.getValue();
+            String language = QuestionsSubmitLanguageEnum.JAVA.getValue();
             List<String> inputList = Arrays.asList("1 2", "3 4");
             ExecuteCodeRequest executeCodeRequest = ExecuteCodeRequest.builder()
                     .code(code)
